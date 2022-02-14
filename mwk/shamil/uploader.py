405863c246d3fb5556f5710978999884d3106a3a
hello universe <87313549+bulanbintang69@users.noreply.github.com>
2022-02-14 17:39:41 +0700

    Update uploader.py

--- mwk/shamil/uploader.py
+++ mwk/shamil/uploader.py
@@ -28,7 +28,7 @@ async def uploader(bot,file, update, msg,as_file=False):
     afilename = file.split("/")[-1]
     filename = " ".join(afilename.split(".")[0:-1])
     if Config.CUSTOM_CAPTION:
-         filename = "»ɴᴀᴍᴀ ꜰɪʟᴇ :\n" + filename + "\n " + Config.CUSTOM_CAPTION
+         filename = "●▬ ❝𝖆𝖓𝖎𝖒𝖊 𝖇𝖆𝖍𝖆𝖘𝖆 𝖎𝖓𝖉𝖔❞▬●\n" + "»ɴᴀᴍᴀ ꜰɪʟᴇ :\n" + filename + "\n" + Config.CUSTOM_CAPTION
     # Thumb Location parameter 
     thumb_location = Config.DOWNLOAD_LOCATION + "/thumb/" + str(update.chat.id) + ".jpg" 
     thumb_image_path = None
